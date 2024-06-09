c16ad675e33fcec27800cbd8c8d12b253f8fea7b
SemKoksharov <samsungj73965@gmail.com>
2024-06-09 23:04:31 +0200

    >- `servizio` e `controller` aggiornati >- creato `DTO` per l'aggiornamento >- modificato il metodo di gestione degli errori (lanciando eccezioni) >- le risposte di controller sono state portate ad una forma generale

--- src/main/java/com/develhope/spring/dtos/requests/SubscriptionRequestDTO.java
+++ src/main/java/com/develhope/spring/dtos/requests/SubscriptionRequestDTO.java
@@ -5,12 +5,14 @@ import com.develhope.spring.entities.Subscription;
 public class SubscriptionRequestDTO {
 
     private Subscription.SubscrType type;
+    private Long listenerId;
 
     public SubscriptionRequestDTO() {
     }
 
-    public SubscriptionRequestDTO(Subscription.SubscrType type) {
+    public SubscriptionRequestDTO(Subscription.SubscrType type, Long listenerId) {
         this.type = type;
+        this.listenerId = listenerId;
     }
 
     public Subscription.SubscrType getType() {
@@ -19,5 +21,13 @@ public class SubscriptionRequestDTO {
 
     public void setType(Subscription.SubscrType type) {
         this.type = type;
+    }
+
+    public Long getListenerId() {
+        return listenerId;
+    }
+
+    public void setListenerId(Long listenerId) {
+        this.listenerId = listenerId;
     }
 }
