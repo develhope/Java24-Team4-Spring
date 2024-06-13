39dff9aae041b6fa9357612ca734a01ce94669ac
Davidetat1 <davidetatone92@gmail.com>
2024-06-13 13:51:44 +0200

    feat:
    creato branch comment

---
+++ src/main/java/com/develhope/spring/dtos/responses/CommentResponseDTO.java
@@ -0,0 +1,56 @@
+package com.develhope.spring.dtos.responses;
+
+import com.develhope.spring.entities.Listener;
+import com.develhope.spring.entities.Song;
+
+public class CommentResponseDTO {
+    private Long id;
+    private Song song;
+    private Listener listener;
+    private String commentText;
+
+    public CommentResponseDTO() {
+    }
+
+    public CommentResponseDTO(Long id, Song song, Listener listener, String commentText) {
+        this.id = id;
+        this.song = song;
+        this.listener = listener;
+        this.commentText = commentText;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public void setSong(Song song) {
+        this.song = song;
+    }
+
+    public Listener getListener() {
+        return listener;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+}
+
+
